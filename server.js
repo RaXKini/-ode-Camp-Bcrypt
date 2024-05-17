22ee7731aaba031954d2f16bd90a99bd68b4ff00
Rahul Kini <93133233+RaXKini@users.noreply.github.com>
2024-05-17 11:17:43 +0530

    Update server.js
    Compare passwords Synchronously...

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
         console.log(res)
     });
@@ -21,39 +21,12 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
-
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
